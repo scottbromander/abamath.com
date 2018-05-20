773f5dff1dd1b478f370d13ac8c6c33fc5e944f4
Luke Schlangen <lukeschlangen@gmail.com>
2018-05-20 16:44:00 -0500

    create sections with h2 headings

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Abamath</h1>
+        <h2>UPCOMING CLASSES</h2>
+        {/* Sam */}
+        <h2>About/FAQ</h2>
+        {/* Madie */}
+        <h2>Code Championship</h2>
+        {/* Roo */}
+        <h2>Contact</h2>
+        {/* Sage */}
       </div>
     );
   }

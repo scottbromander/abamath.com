8ccd22a59b65ad13976d238670bfc62f9551fdcf
Sam Woollums <woollumss@gmail.com>
2018-07-11 11:53:44 -0500

    add react-helmet and 404

---
+++ src/pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+const NotFoundPage = () => (
+  <div>
+    <h1>NOT FOUND :(</h1>
+    <p>You just hit a page that doesn't exist.</p>
+  </div>
+)
+export default NotFoundPage

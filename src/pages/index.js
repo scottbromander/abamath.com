30cb50e69d3ff21a0eaa59ef4fb00d6f89c2a6cd
ivers598 <ivers598@umn.edu>
2018-07-12 16:12:59 -0500

    trash

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,29 +10,6 @@ export default ({ data }) => {
       <g.H1 display={"inline-block"} borderBottom={"1px solid"}>
         abamath
       </g.H1>
-      
-
-     
-      <h2>
-        Districts
-      </h2>
-      {data.districts.edges.map(({ node }) =>
-        <div key={node.id}>
-          <Link
-            to={node.fields.slug}
-            css={{ textDecoration: `none`, color: `inherit` }}
-          >
-
-            <g.H3 marginBottom={rhythm(1 / 4)}>
-              {node.frontmatter.title}{" "}
-              <g.Span color="#BBB">— {node.frontmatter.location}</g.Span>
-            </g.H3>
-            <p>
-              {node.excerpt}
-            </p>
-          </Link>
-        </div>
-      )}
       {data.allCommunityEducationClasses.edges.map(({ node }) =>
         node.fields && node.fields.slug ? <div key={node.id}>
           <Link
@@ -41,12 +18,11 @@ export default ({ data }) => {
           >
 
             <g.H3 marginBottom={rhythm(1 / 4)}>
-              {node.fields.className}{" "}
+              {node.fields.className}{" "} &nbsp;
+              {node.fields.days}{" "}&nbsp;
+              {node.fields.grades}{" "} &nbsp;
               {/* <g.Span color="#BBB">— {node.frontmatter.date}</g.Span> */}
             </g.H3>
-            <p>
-              {node.excerpt}
-            </p>
           </Link>
         </div> : ''
       )}
@@ -56,57 +32,6 @@ export default ({ data }) => {
 
 export const query = graphql`
   query IndexQuery {
-    blogs: allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {fileAbsolutePath: {regex: "*/blog/.*\\.md$/"}}) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            location
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    },
-    classes: allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {fileAbsolutePath: {regex: "*/classes/.*\\.md$/"}}) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            location
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    },
-    districts: allMarkdownRemark(sort: {order: DESC, fields: [frontmatter___date]}, filter: {fileAbsolutePath: {regex: "*/districts/.*\\.md$/"}}) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date(formatString: "DD MMMM, YYYY")
-            location
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
     allCommunityEducationClasses {
       totalCount
       edges {

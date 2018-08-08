0815db2d9659704a7b50470b8fc88524ecb7e244
Sam Woollums <swoollums@Sams-MacBook-Air.local>
2018-08-08 11:48:50 -0500

    add animated placeholder

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,19 @@ import "./index.css";
 export default class Index extends React.Component {
     state = {
     searchText: '',
+    placeholderList: ["Search Here", "Hopkins", "Search Here", "Coding", "Search Here", "Minnetonka", "Search Here", "Edina"],
+    place: 0,
+    placeholder: '',
+    char: 0,
+    rm: 0,
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.updatePlaceholder, 150)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   updateSearchText = (event) => {
@@ -22,6 +35,37 @@ export default class Index extends React.Component {
     });
   }
 
+  updatePlaceholder = () => {
+    if (this.state.rm == 1) {
+        this.setState({placeholder: this.state.placeholder.substring(0, this.state.char) + '|', char: this.state.char-1})
+      if (this.state.placeholder == '|') {
+        if (this.state.place == this.state.placeholderList.length-1) {
+          this.setState({placeholder: '', place: 0, rm: 0, char: 0})
+        }
+        else {
+          this.setState({placeholder: '', place: this.state.place+1, rm: 0, char: 0,})
+        }
+      }
+    }
+    else {
+      if (this.state.placeholder.length-1 == this.state.placeholderList[this.state.place].length) {
+        setTimeout(() => {
+            this.setState({
+            placeholder: this.state.placeholder,
+            rm: 1
+          })
+        }, 700)
+      }
+      else {
+        this.setState({ 
+          placeholder: this.state.placeholder.substring(0, this.state.char) + this.state.placeholderList[this.state.place][this.state.char] + '|',
+          char: this.state.char+1,
+        })
+      }
+    }
+  }
+
+
   render() {
     return (
       <div>
@@ -29,7 +73,7 @@ export default class Index extends React.Component {
         <div id="body">
           <div id="classes">
           <h2>UPCOMING CLASSES</h2>
-          <input type="text" placeholder="Search..." onChange={this.updateSearchText} value={this.state.searchText} />
+          <input type="text" placeholder={this.state.placeholder} onChange={this.updateSearchText} value={this.state.searchText} />
           <div id="table">
           <ClassTable
             districtClasses={this.props.data.allCommunityEducationDistrictClasses.edges}

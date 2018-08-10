a3ec100ca7c150c1de23bd44cb56d3fc762be697
Madie Martens <madiemm97@gmail.com>
2018-08-10 13:24:48 -0500

    add components for coding for kids class

--- src/components/curriculum/ClassroomProcedures.js
+++ src/components/curriculum/ClassroomProcedures.js
@@ -24,8 +24,7 @@ export default class ClassroomProcedures extends React.Component {
                       doesn’t know exactly what to do when I say go?” Then pause yet again. </li>
                       </ul>
         <p>Way to get attention:</p>
-                    <ul>
-
+                <ul>
                     <li>Say, “1 2 3, eyes on me” and they respond, “1 2, eyes on you”</li> 
                     <li>With the younger kids, have them make mouth bubbles/”put a marshmallow 
                         in your mouth” where the kids inflate their cheeks. It gets their attention 
@@ -80,7 +79,7 @@ export default class ClassroomProcedures extends React.Component {
                         bathroom first so that they have that time to calm down before walking back 
                         into the classroom. </li>
                     </ul>
-            <p>A kid having a bad attitude the whole class</p>
+            <h4>A kid having a bad attitude the whole class</h4>
                 <ul>
                     <li>First, tell them you will take away privileges, like computer time, break time, 
                         talking to their parents, or talking to another school official/camp official. </li>
@@ -95,13 +94,206 @@ export default class ClassroomProcedures extends React.Component {
                     <li>Red/Green Card - A student needs to ask if they would like to talk by turning 
                         the card to green. The instructor can give the student the floor or turn the 
                         card back to red (can use a playing card if that is easier/on hand)</li>
-
-
-
-
                     </ul>
-            
+            <h4>Swearing or inappropriate comments</h4>
+            <ul>
+                <li>First incident: Warning. Let them know that those words may be okay in a 
+                    different setting, but not this one. </li>
+                <li>Second incident: Warning with note or call home </li>
+                <li>Tips:</li>
+                    <ul>
+                    <li>“We don’t talk like that to each other.” </li>
+                    <li>“In this classroom we use formal language. The word you used is considered a 
+                        swear room in my classroom and I don’t want to hear it again.” </li>
+                    <li>Ask them a different way to express their thought</li>
+                    <li>“Language please!”</li>
+                    </ul>
+            </ul>
+            <h4>How do we handle a child's misuse of the internet? </h4>
+                <p>*** Use as a guideline! Situations may vary, especially with kids who have ADHD 
+                    or other conditions, so make sure to account for those factors ***</p>
+                <ul>
+                    <li>Less severe </li>
+                        <ul>
+                        <li>First incident: warning</li>
+                        <li>Second incident: warning with note or call home </li>
+                        <li>Third incident: warning with note or call home </li>
+                        <li>Fourth incident: termination of class </li>
+                        </ul>
+                    <li>More severe </li> 
+                        <ul>
+                            <li>First incident: warning with note or call home</li>
+                            <li>Second incident: termination of class </li>
+                        </ul>
+                </ul>
+            <h4>What do we do if it does seem like one kid is being bullied? </h4>
+                <ul>
+                <li><a href="https://www.smartclassroommanagement.com/2011/06/25/bullying-in-the-classroom/">Resource on Bullying</a> </li>
+                <li>Know what bullying is: “Bullying is the strong preying upon the weak. It can be 
+                    a physical advantage or a social advantage. It can be one student or many. It 
+                    often takes the form of threats, intimidation, repeated cruelty, and/or forcing 
+                    someone against his or her will to do what the bully wants.” </li>
+                <li>Separate bully from those bullied immediately: “Once you’re clear on the facts 
+                    of the incident, and know who is responsible, act immediately. Separate the bully 
+                    or bullies from their classmates in an in-class time-out. Tell them why, but don’t
+                     lecture, scold, raise your voice, or even ask for a response.” </li>
+                <li>Speak to the bully’s parents and the victim’s parents. </li>
+                <li>Keep a close eye on both the bully and the victim for the rest of the week. </li>
+                </ul>
+            <h4>What if a child is not at all interested? (Ex. Just wants to sit in the corner 
+                    and read) </h4>
+                <ul>
+                <li>Really tough to answer. Situational. Ideally, finding something the student can 
+                    be excited by. At end of first day, discussing with parent. </li>
+                <li>These kids will have phones. What if there is a prolonged amount of use on them? </li>
+                <li>Only allowed use during breaks</li>
+                <li>It may be best to have a strategy of collecting phones at the beginning of 
+                    class and then returning them during breaks. Luke is okay with requiring the 
+                    student to give up their phone if they are unable to refrain from using it. </li>
+                </ul>
+            <h4>These kids will have phones. What if there is a prolonged amount of use on them? </h4>
+                <ul>
+                    <li>Only allowed use during breaks</li>
+                    <li>It may be best to have a strategy of collecting phones at the beginning of 
+                        class and then returning them during breaks. Luke is okay with requiring the 
+                        student to give up their phone if they are unable to refrain from using it. </li>
+                </ul>
         
+        <h3>Parent Policies</h3>
+            <h4>What if a parent is being a bother? What if they want to sit in and watch us teach? </h4>
+            <ul>
+                <li>For safety reasons, I would suggest no parents are allowed to sit in and watch 
+                    unless Luke is present. Someone needs to be around to “escort” him or her, and 
+                    with an instructor teaching, that would leave us out of that position to do so. 
+                    We never know what kind of dangers or situations could arise from a parent. We can 
+                    kindly tell them that there is an opportunity to see what their child is doing on
+                     Thursday when they can come in and see what they did. Also tell them that it 
+                     is for safety purposes that they cannot sit in. </li>
+                <li>Alternative answer: Let them stay for 30 minutes tops. From my own experience,
+                     I feel uncomfortable when a parents tries to watch me teach. It feels like they 
+                     are stepping on my toes and like I no longer have authority over the class (could 
+                     be an age thing). </li>
+            </ul>
+                
+                
+        <h3>Checkout Policies</h3>
+            <ul>
+                <li>Site for check ins: <a href = "https://abamath-check-in.herokuapp.com/#!/" >here</a> </li>
+                <li>At the beginning of class, you should know/ask a district official how the 
+                    check-in/checkout system of the district works. Some districts have a camp system 
+                    that will determine where you pick up and drop off the kids.</li>
+                <li>For districts without a camp system, the kids will most likely be dropped off 
+                    in the classroom by the parents. You should determine by the first parent where
+                     you will drop off the kids/have them picked up in the classroom to keep consistency
+                      for the remaining parents. </li>
+                <li>At the end of the day, you must make sure the students’ spaces are clean.</li>
+                <li>If the parents pick up the child in the classroom, you and the other instructor(s)
+                     wait for every child to be picked up from the classroom </li>
+                <li>If you walk the kids to a designated area, make sure you and the other 
+                    instructor(s) walk the kids to that location together. You and the other 
+                    instructor(s) must stay and watch to make sure every child gets picked up by
+                     the correct car.</li>
+            </ul>
+            <h4>If we have a bad feeling about who is picking a child up.</h4>
+                <ul>
+                    <li>Parents should have to sign their child in and out unless they write a note 
+                    with their signature saying otherwise. To pick up a child, a parent or guardian 
+                    should have their ID (remind them of this when they drop off their kid), and on our
+                     roster we should have a list of eligible pick-up people. If someone comes in and 
+                     tries to pick up their child and their name is not on the list, we call someone 
+                     who is on the list to confirm if it is okay or not. Instructor or parent then 
+                     writes down the time the child arrives and leaves. This could be helpful for 
+                     legal reasons if we are blamed for something during a time we were not in charge 
+                     of the kid. Safety should be our number one priority and although it may be more
+                      of a hassle for parents and us as well, I think they will appreciate our attention
+                       to detail and safety in doing so. </li>
+                    <li>Get a school official, if necessary. It is always safe to have multiple people
+                         involved in a situation like this.</li>
+                    <li>Definitely call police if we have a bad feeling about someone picking a child up. 
+                        If you have a co-instructor, ask for their judgment on the situation as well 
+                        before calling. </li>
+                </ul>
+        <h3>Emergency Policies</h3>
+            <h4>First aid</h4>
+                <ul><li>I think CPR/First Aid training would be a good investment for this company to make
+                     with their instructors. It’s possible some people have an up-to-date certification 
+                     already. I believe it’s as crucial as running a background check. </li></ul>
+            <h4>First aid examples</h4>
+                <ul>
+                    <li>Tweezers - use to take out splinters or bee stingers </li>
+                    <li>Ice pack - use if a child is complaining of a pain or after they get stung 
+                        by a bee </li>
+                    <li>Bandaids - use for minor scrapes and bumps </li>
+                    <li>Hydrocortisone - use for minor pains, swelling, itching, and discomfort </li>
+                </ul>
+            <h4>Emergency situations - if there was a fire, tornado, etc. </h4>
+                <ul>
+                    <li>Typically, rosters can be found on Eleyo. Every instructor should print a hard 
+                        copy roster of their class with names, allergies/important information, emergency
+                         contact information, and parents/guardian names before the class starts. </li>
+                    <li>In case of a fire, escort your class to a safe location and 
+                        count your kids / check your roster when you arrive. Let someone know if you 
+                        don’t have your whole class. Never leave the kids alone. By having parents sign 
+                        their child in and out, we can quickly see if a child didn’t come that day or 
+                        if they are indeed somewhere else. </li>
+                    <li>In case of a tornado, escort your class to a safe location and count your
+                         kids / check your roster when you arrive. Let someone know if you don’t have
+                          your whole class. Never leave the kids alone. By having parents sign their 
+                          child in and out, we can quickly see if a child didn’t come that day or if 
+                          they are indeed somewhere else. </li>
+                    <li>In case of an active shooter situation, your best option is leave the building 
+                        and don’t be a sitting duck. Getting away from the shooter or shooters is top 
+                        priority. Leave everything except your roster behind. Help other escape, if 
+                        possible, but evacuate regardless of if others agree to follow. Prevent your 
+                        class from going where the active shooter may be. Call 911 when you are safe. </li>
+                    <li>If leaving isn’t an option, hide. Tell your class to stay very quiet and ensure 
+                        that all phones are off and won’t vibrate. Lock and block doors, close blinds, 
+                        and turn off the lights. Don’t hide in groups; spread out along wall or hide 
+                        separately to make it harder for the shooter. Try to communicate silently with 
+                        the police by sending a text or tagging your location on social media. Stay in
+                         place until you get the all clear from officials. </li>
+                </ul>
+            <h4>Lost child procedure?</h4>
+                    <li>Hasn’t happened. Immediate outreach to community education. Could depend on 
+                        how/when it happened. </li>
+
+
+        <h3>Other Policies</h3>
+            <h4>Bathroom procedures and rules</h4>
+                <ul>
+                    <li>Try to bring the whole group during designated break time (at least one every
+                         90 minutes).</li>
+                    <li>If there are 2 instructors and it’s not break time, one instructor can ask for 
+                        a bathroom call. Always bring more than one child even if only one has to go. 
+                        If it’s an emergency and it’s only you, bring the whole class. </li>
+                    <li>Never go into a stall with a child, even with it unlocked. </li>
+                    <li>Shouldn’t be a problem with our age groups, but if a younger child is having a 
+                        hard time zipping or buttoning their pants, try to talk them through it. If there
+                         is an absolute need for help, have another adult be a spotter. This could help
+                          in legal situations. </li>
+                </ul>
+            <h4>What do we do with extra time? </h4>
+                <ul><li>Typically extend free work time, or look to ICE plans</li></ul>
+
+            <h4>Printing Policy</h4>
+                <ul>
+                    <li>Who prints: The person in charge of printing will be assigned for the summer</li>
+                    <li>Where to print: The printing person may choose a location to print (ie local 
+                        library or FedEx/OfficeMax), location should be used consistently</li>
+                    <li>One-time prints: For one time prints (eg a new activity/curriculum update), 
+                        email the document and number of copies needed to the printing person. 
+                        These will be distributed at Team Tuesday meeting </li>
+                    <li>Recurring prints(eg end of class handouts/flyers): </li>
+                        <ul>
+                            <li>Strategy 1: Each week, on Friday, instructors should email the printing
+                                 person with the documents they need and the number of copies(based on 
+                                 their roster and including copies for potential last minute adds). 
+                                 These will be distributed on Team Tuesday</li>
+                            <li>Strategy 2: Have the printing person do the work outlined above for all
+                                 instructors for the week</li>
+                        </ul>
+                </ul>
+
       </div>
     )
   }

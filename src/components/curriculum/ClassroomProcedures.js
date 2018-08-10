1fc866d1fe8bc1d8cbbee6678025563dd91d6f57
Madie Martens <madiemm97@gmail.com>
2018-08-10 08:53:49 -0500

    add classroom procedures

--- src/components/curriculum/ClassroomProcedures.js
+++ src/components/curriculum/ClassroomProcedures.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { OutboundLink } from 'gatsby-plugin-google-analytics';
 import Link from "gatsby-link"; 
+import "./curriculum.css"; 
 
 <meta></meta>
 
@@ -11,6 +12,96 @@ export default class ClassroomProcedures extends React.Component {
         <h2>
           Classroom Procedures 
         </h2>
+        <h3>Behavior Policies</h3>
+        <h4>Important Teaching Concepts That Will Help in the Classroom</h4> 
+        <p>Preview the class </p>
+            <ul> 
+                <li>Example: “We are taking a break in TWO minutes!”</li>
+                <li>Example: “When I say go you’re going to stand up, push in your chair, and line up 
+                    at the door. From there, we are going to take a bathroom break as a class followed
+                     by our 15 to 20 minute break outside.” Before giving your ‘go’ signal, pause a moment
+                      to let their mental movie play out. Then challenge your class. “Is there anyone who 
+                      doesn’t know exactly what to do when I say go?” Then pause yet again. </li>
+                      </ul>
+        <p>Way to get attention:</p>
+                    <ul>
+
+                    <li>Say, “1 2 3, eyes on me” and they respond, “1 2, eyes on you”</li> 
+                    <li>With the younger kids, have them make mouth bubbles/”put a marshmallow 
+                        in your mouth” where the kids inflate their cheeks. It gets their attention 
+                        and stops them from talking over you</li>
+                    <li>What color are my eyes/glasses/etc.?</li> 
+                    <li>“If you hear me clap once… If you hear me clap twice… If you hear my clap three
+                         times…” </li>
+                    <li>Count backwards from 4</li>
+                    <li>Have students stand and stretch, then sit and get started </li>
+                    <li>“The noise level so far has cost us two minutes of break time.” 
+                        Then hold up the number of minutes they have lost and wait for the 
+                        silence, adding more minutes if it continues a long time. </li> 
+                    <li>Flash lights </li> 
+                    <li> For elementary schoolers, if you say X, they say Y</li>
+                        <ul>
+                            <li>“All on board → Aye aye captain!” (kids can salute)</li>
+                            <li>“Ready to rock → Ready to roll!” (kids can fist pump)</li>
+                            <li>“Are you ready? → I was born ready!” (kids can make karate pose)</li>
+                        </ul>
+                    <li>Do what I do / Simon says: Have students continue to do what you do until you 
+                        have all of their attention (pat head, rub stomach, tap ears, etc) </li> 
+
+                        </ul>
+            <p>How to Transition a Class</p>
+                <ul>
+                    <li>Simple Procedure</li> 
+                    <ul> 
+                        <li>Secure students’ attention: “Focus on me please” </li>
+                        <li>Explain the procedure: “In a moment, we are going to transition to 
+                            break time.”</li>
+                        <li>Prepare kids for the signal to start: “When I say ‘smooth,’ you’ll 
+                            quietly proceed.” </li>
+                        <li>Initiate the transition: “And...smooth.” Don’t say ‘go’ because that 
+                            word cues students to race. </li>
+                        <li>Observe: Watch to make sure all students are complying. </li>
+
+                        </ul>
+
+                    <li>Slowly counting down from five never fails to accelerate transitions. </li>
+                    <li>Right before a transition, ask a student to describe the proper transition
+                         procedure. </li>
+                    <li>Play “correct the teacher” and perform examples and nonexamples of behavior
+                         while the students indicate if something is wrong by putting a thumbs down. 
+                         Then call on someone to articulate the right procedure. </li>
+                    <li>When students don’t want to quit what they’re doing, it’s probably because 
+                        they felt blindsided. This can be corrected and prevented by having a visible
+                         countdown timer as well as verbal reminders of when transitions will happen. </li>
+                    <li>You could play a song like “I like to move it, move it” to signal that it is
+                         time to change activities. </li>
+                    <li>Give students a time limit for transitions</li>
+                    <li>When transitioning back to the classroom, stop at the drinking fountain and 
+                        bathroom first so that they have that time to calm down before walking back 
+                        into the classroom. </li>
+                    </ul>
+            <p>A kid having a bad attitude the whole class</p>
+                <ul>
+                    <li>First, tell them you will take away privileges, like computer time, break time, 
+                        talking to their parents, or talking to another school official/camp official. </li>
+                    <li>If the situation worsens, then start to follow through on taking these privileges
+                         away and/or contacting their parents or a school official/camp official.</li>
+                    <li>Take a break chair - Not a timeout, because the time doesn’t matter. The point 
+                        is to allow the student to take time to think about why they needed a break. 
+                        When they come back, they need to discuss what ideas they have to help them 
+                        stay a part of the class and stay on task. </li>
+                    <li>Deep Breaths</li>
+                    <li>Coloring</li>
+                    <li>Red/Green Card - A student needs to ask if they would like to talk by turning 
+                        the card to green. The instructor can give the student the floor or turn the 
+                        card back to red (can use a playing card if that is easier/on hand)</li>
+
+
+
+
+                    </ul>
+            
+        
       </div>
     )
   }

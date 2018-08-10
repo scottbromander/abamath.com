c058004cfa1f2a4ef65eb2c48eb27d4b113f6c92
Sam Woollums <swoollums@Sams-MacBook-Air.local>
2018-08-10 18:08:14 -0500

    some styling of classes page

--- src/templates/district-class.js
+++ src/templates/district-class.js
@@ -21,6 +21,7 @@ export default class DistrictClass extends React.Component {
     const specificClass = this.props.data.specificClass.edges[0].node.fields;
     return (
       <div id="district-classes">
+          <div id="classinfo" >
           <h1>{specificClass.district} {specificClass.className}</h1>
           <h2>Time: {specificClass.time}</h2>
           <h2>Grades: {specificClass.grades}</h2>
@@ -28,15 +29,16 @@ export default class DistrictClass extends React.Component {
           <h2>Dates: {specificClass.startdate} - {specificClass.enddate}</h2>
           <p> {specificClass.description}</p>
 
-          <OutboundLink href={specificClass.link}><button>Sign up!</button></OutboundLink>
+          <OutboundLink href={specificClass.link}><button >Sign up!</button></OutboundLink>
+          </div>
 
-        <h2>Other classes in {specificClass.district}{""}</h2>
+        <h3>Other Camps in {specificClass.district}{""}</h3>
         <ClassTable
           districtClasses={this.props.data.allDistrictClasses.edges}
           searchText={this.state.searchText}
         />
 
-        <h2>Other {specificClass.className}{""} Camps</h2>
+        <h3>Other {specificClass.className}{""} Camps</h3>
         <ClassTable
           districtClasses={this.props.data.allDistrictClasses.edges}
           searchText={specificClass.className}

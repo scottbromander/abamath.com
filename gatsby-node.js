cd1094f180f67f12007e980ed663f6963fbbdd32
Luke Schlangen <lukeschlangen@gmail.com>
2018-08-14 21:05:57 -0500

    break function generators into utils modules

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,10 @@
-const path = require(`path`);
-const { correctGoogleSheetRow, validateGoogleSheetRowObject } = require('./utils/google-sheet-corrector');
-const slugify = require('slugify');
+const { pagePromiseGenerator } = require('./utils/page-promise-generator');
+const { googleSheetNodeFilter } = require('./utils/google-sheet-node-filter');
 const { createFilePath } = require('gatsby-source-filesystem');
 
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
   const { createNodeField, deleteNode } = boundActionCreators
-  const googleSheetNodeFieldCreator = googleSheetRowFilter(node, createNodeField, deleteNode);
+  const googleSheetNodeFieldCreator = googleSheetNodeFilter(node, createNodeField, deleteNode);
   const isMarkdownRemark = node.internal.type === 'MarkdownRemark';
   const isDistrictClass = node.internal.type === 'community_education__district_classes';
   const isOfferedClass = node.internal.type === 'community_education__offered_classes';
@@ -43,7 +42,7 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
 
 exports.createPages = ({ graphql, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
-  const slugRoutePageCreator = slugRoutePagePromiseGenerator(graphql, createPage);
+  const slugRoutePageCreator = pagePromiseGenerator(graphql, createPage);
   return Promise.all([
     slugRoutePageCreator('allMarkdownRemark', './src/templates/markdown-display.js'),
     slugRoutePageCreator('allCommunityEducationDistrictClasses', './src/templates/district-class.js'),
@@ -51,63 +50,3 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
     slugRoutePageCreator('allCommunityEducationDistrict', './src/templates/district.js'),
   ]);
 };
-
-const googleSheetRowFilter = (node, createNodeField, deleteNode) => (titleName, requiredFields) => {
-  const correctedGoogleSheetRow = node.content && correctGoogleSheetRow(node.content._t);
-  const isValidGoogleSheetRow = correctedGoogleSheetRow && validateGoogleSheetRowObject(correctedGoogleSheetRow, requiredFields);
-  if (isValidGoogleSheetRow) {
-    const rowTitle = node.title._t;
-    const pageTitle = correctedGoogleSheetRow.district ? correctedGoogleSheetRow.district + " " + rowTitle : rowTitle;
-
-    createNodeField({
-      node,
-      name: titleName,
-      value: rowTitle,
-    });
-    createNodeField({
-      node,
-      name: 'slug',
-      value: '/' + slugify(pageTitle.toLowerCase()),
-    });
-    Object.entries(correctedGoogleSheetRow).forEach((entry) => {
-      createNodeField({
-        node,
-        name: entry[0],
-        value: entry[1],
-      });
-    });
-  } else {
-    deleteNode(node.id, node);
-  }
-}
-
-const slugRoutePagePromiseGenerator = (graphql, createPage) => (gqlNodeName, pageComponent) => new Promise((resolve, reject) => {
-  graphql(`
-  {
-    ${gqlNodeName} {
-      totalCount
-      edges {
-        node {
-          fields {
-            slug
-          }
-        }
-      }
-    }
-  }    
-`).then(result => {
-      result.data && result.data[gqlNodeName] && result.data[gqlNodeName].edges.forEach(({ node }) => {
-        if (node.fields && node.fields.slug) {
-          createPage({
-            path: node.fields.slug,
-            component: path.resolve(pageComponent),
-            context: {
-              // Data passed to context is available in page queries as GraphQL variables.
-              slug: node.fields.slug,
-            },
-          });
-        }
-      });
-      resolve();
-    });
-});

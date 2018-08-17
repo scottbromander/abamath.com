844c91397eaf5aac7b9b934d4ea9bcd144ee3ce7
Sam Woollums <swoollums@Sams-MacBook-Air.local>
2018-08-17 15:31:10 -0500

    fix

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,25 +4,25 @@ import "./navigation.css"
 import Link from "gatsby-link";
 
 export default class Navigation extends React.Component {
+  showNav() {
+    var x = this.refs.myTopnav
+    if (x && x.className === "topnav") {
+      x.className += " responsive";
+    } 
+    else if (x) {
+      x.className = "topnav";
+    }
+  }
   
   render(){
-    function showNav() {
-      var x = document.getElementById("myTopnav")
-      if (x && x.className === "topnav") {
-        x.className += " responsive";
-      } 
-      else if (x) {
-        x.className = "topnav";
-      }
-    }
     return(
-      <div className="topnav" id="myTopnav">
+      <div className="topnav" id="myTopnav" ref="myTopnav">
         <Link to={'/'} className="nav-left"><img src={logo} /></Link>
         <Link to={'/'} className="nav-left"><h1>abamath</h1></Link>
         <Link to={'/#contact'} className="nav-right">Contact</Link>
         <Link to={'/#about'} className="nav-right">About</Link>
         <Link to={'/#classes'} className="nav-right">Camps</Link>
-        <Link to={showNav()} className="icon"><span></span><span></span><span></span></Link>
+        <Link to={this.showNav()} className="icon"><span></span><span></span><span></span></Link>
 
     </div>
     )

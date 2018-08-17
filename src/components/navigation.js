90fc5a535830bf7ad4441577c1ba118c3fcd01ff
Sam Woollums <swoollums@Sams-MacBook-Air.local>
2018-08-17 15:16:37 -0500

    add nav bar and table mobile styling

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,62 +1,30 @@
 import React from "react";
-import { OutboundLink } from 'gatsby-plugin-google-analytics';
 import logo from "../images/abamath.png"
 import "./navigation.css"
 import Link from "gatsby-link";
 
 export default class Navigation extends React.Component {
-
+  
   render(){
+    function showNav() {
+      var x = document.getElementById("myTopnav")
+      if (x && x.className === "topnav") {
+        x.className += " responsive";
+      } 
+      else if (x) {
+        x.className = "topnav";
+      }
+    }
     return(
-      <div id="navigation">
-        <ul>
-      <li>
-        <Link to={`/`}>
-            <img src={logo} />
-        </Link>
-      </li>
-      <li>
-        <Link to={`/`}>
-          <h1>abamath</h1>
-        </Link>
-      </li>
-      {/* <li>
-      <Link className={linkStyle} to={`/contact/`}>
-        Contact 
-      </Link> 
-      </li>
-      <li>
-      <Link className={linkStyle} to={`/curriculum/`}>
-        Camps 
-      </Link>
-      </li>
-      <li>
-      <Link className={linkStyle} to={`/about/`}>
-        About 
-      </Link>
-      </li>
-      <li>
-      <Link className={linkStyle} to={`/`}>
-        Home
-      </Link> 
-      </li> */}
-      <li className="pages">
-        <Link to={`/#contact`}>
-          Contact
-        </Link>
-      </li>
-      <li className="pages">
-        <Link to={`/#about`}>
-          About
-        </Link>
-      </li>
-      <li className="pages">
-        <Link to={`/#classes`}>
-          Camps
-        </Link>
-      </li>
-      </ul>
-      </div>
+      <div className="topnav" id="myTopnav">
+        <Link to={'/'} className="nav-left"><img src={logo} /></Link>
+        <Link to={'/'} className="nav-left"><h1>abamath</h1></Link>
+        <Link to={'/#contact'} className="nav-right">Contact</Link>
+        <Link to={'/#about'} className="nav-right">About</Link>
+        <Link to={'/#classes'} className="nav-right">Camps</Link>
+        <Link to={showNav()} className="icon"><span></span><span></span><span></span></Link>
+
+    </div>
     )
   }
 }

1659a1b3da1d6850a7d854d74d65e2f33cf47655
Luke Schlangen <lukeschlangen@gmail.com>
2018-08-29 17:32:28 -0500

    fixing upcoming id

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,7 +45,7 @@ export default class Index extends React.Component {
       <div>
         <div id="background"></div>
         <div id="body">
-          <div id="classes">
+          <div id="upcoming">
             <img src={upcomingCampsIcon} alt="Upcoming Camps" />
             <h2>Upcoming Camps</h2>
             <InputHints

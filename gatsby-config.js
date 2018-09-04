25005155de6898b1e9f8bcdb988e3dce96cd60ea
Luke Schlangen <lukeschlangen@gmail.com>
2018-09-04 10:21:07 -0500

    add anonymizing ip

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
               // Puts tracking script in the head instead of the body
               head: false,
               // Setting this parameter is optional
-              // anonymize: true,
+              anonymize: true,
               // Setting this parameter is also optional
               respectDNT: true,
             },

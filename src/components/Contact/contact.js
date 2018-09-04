94fde2d5a3e62223c3369749fc5856180698f1a0
Luke Schlangen <lukeschlangen@gmail.com>
2018-09-04 11:31:01 -0500

    move upcoming classes to separate component

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -5,11 +5,11 @@ import "./contact.css"
 export default class Contact extends React.Component {
   render(){
     return(
-      <div id="contact">
+      <section className="homepage-section"  id="contact">
         <h2>Connect With Us</h2>
         <p>Want to bring abamath to your school, find out what we're doing next, or just say hi?</p>
         <p><OutboundLink href="mailto:info@abamath.com">info@abamath.com</OutboundLink></p>
-      </div>
+      </section>
     )
   }
 }

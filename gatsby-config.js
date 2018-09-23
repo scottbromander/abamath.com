f6692e37a507f5efa93cdbf6e28f5ad2848550b5
Luke Schlangen <lukeschlangen@gmail.com>
2018-09-23 10:20:26 -0500

    add domine font

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
       options: {
         fonts: [
           'Jura',
+          'Domine',
         ],
       },
     },
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -3,6 +3,23 @@ import Typography from 'typography';
 const typography = new Typography({
   headerFontFamily: ['Jura', 'sans-serif'],
   bodyFontFamily: ['Arial', 'serif'],
+  overrideStyles: () => ({
+    'h2, h3': {
+      padding: '1%',
+      margin: '1.5%',
+      textAlign: 'center',
+    },
+    h2: {
+      fontSize: '190%',
+    },
+    h3: {
+      fontSize: '120%',
+    },
+    p: {
+      color: 'black',
+      padding: '2%',
+    },
+  }),
 });
 const { rhythm, scale } = typography;
 export { rhythm, scale, typography as default };

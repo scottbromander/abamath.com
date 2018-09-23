b2c1bf602b272ac1c1c2a5051dde07a65584d897
Luke Schlangen <lukeschlangen@gmail.com>
2018-09-23 09:42:18 -0500

    move font family to typography file

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,8 @@
 import Typography from 'typography';
 
-const typography = new Typography();
+const typography = new Typography({
+  headerFontFamily: ['Jura', 'sans-serif'],
+  bodyFontFamily: ['Arial', 'serif'],
+});
 const { rhythm, scale } = typography;
 export { rhythm, scale, typography as default };
